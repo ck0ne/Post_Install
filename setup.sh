3a6364e38b77f5dc6882cf43539d101a3548e7fc
ck0ne <ck0ne@users.noreply.github.com>
2014-07-09 22:00:00 +0200

    Update setup.sh
    Ajout de DropBox.

--- setup.sh
+++ setup.sh
@@ -2,13 +2,14 @@
 
 cd $home
 
-
 # installation de Filezilla
-
 apt-get install filezilla
 
 # installation de putty
-
 apt-get install putty
 
+# installation de  Dropbox
+wget -O - "https://www.dropbox.com/download?plat=lnx.x86" | tar xzf -
 
+# execution du démon drpobox en arrière plan
+~/.dropbox-dist/dropboxd &
